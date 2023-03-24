d267f6f5e1cf76c316f9241cb3d6b07ded56052c
parulsahu98 <118402695+parulsahu98@users.noreply.github.com>
2023-03-24 10:31:01 +0530

    Update redis.js

--- redis.js
+++ redis.js
@@ -1,30 +1,26 @@
-// const redis = require('redis')
-// const client = redis.createClient({
-//     host:'redis://red-cge4g6qk728lv80vepig',
-//     port:6379
-//   })
+const redis = require('redis')
+const client = redis.createClient({
+    host:'red-cge4g6qk728lv80vepig',
+    port:6379
+  })
 
 
 
 
 
-const redis = require('redis');
+// const redis = require('redis');
 
-(async () => {
-  // Connect to your internal Redis instance using the REDIS_URL environment variable
-  // The REDIS_URL is set to the internal Redis URL e.g. redis://red-343245ndffg023:6379
-  const client = redis.createClient({
-      url: 'red-cge4g6qk728lv80vepig:6379'
-  });
+// (async () => {
+//   // Connect to your internal Redis instance using the REDIS_URL environment variable
+//   // The REDIS_URL is set to the internal Redis URL e.g. redis://red-343245ndffg023:6379
+//   const client = redis.createClient({
+//       url: 'red-cge4g6qk728lv80vepig:6379'
+//   });
 
-  client.on('error', (err) => console.log('Redis Client Error', err));
+//   client.on('error', (err) => console.log('Redis Client Error', err));
 
-  await client.connect();
+//   await client.connect();
 
-  // Send and retrieve some values
-//   await client.set('key', 'node redis');
-//   const value = await client.get('key');
 
-//   console.log("found value: ", value)
-})();
+// })();
   module.exports =client;
